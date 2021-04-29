1be0f1857ee015bc185ee6f94a81c33825e9f0f4
elonifrah <79987872+elonifrah@users.noreply.github.com>
2021-04-29 19:57:15 +0300

    ex4

---
+++ exercises/ex4/get_params.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+
+<body>
+    <?php
+    $userName = $_GET["userName"];
+    $password = $_GET["password"];
+
+    if ($userName=="Beitar" && $password=="LoveBeitar")
+        echo "<h1> Welcome ".$userName."</h1>";
+    else {
+        echo "<h1 style='color:red;'> NO You are Hapoel! You are not, ".$userName."</h1>";
+        echo "<h1> Try UserName:Beitar, Password:LoveBeitar</h1>";
+    }
+    ?>
+</body>
+
+</html>
